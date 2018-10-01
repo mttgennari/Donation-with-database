65c6b5d769cded3cd665365bfd21c90b0f19755f
mttgennari <43754001+mttgennari@users.noreply.github.com>
2018-10-01 17:17:53 +0200

    Add files via upload

---
+++ Persone.sql
@@ -0,0 +1,34 @@
+/* Table data export for table Persone */
+
+/* Preserve session variables */
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS;
+SET FOREIGN_KEY_CHECKS=0;
+
+/* Export data */
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (1,"Matteo","Gennari");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (3,"Riccardo","Giovannini");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (4,"Francesco","Gennari");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (5,"Filippo","Alquati");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (6,"Angelica","Badoni");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (7,"Cecilia","Giusti");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (8,"Veronica","Ferri");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (9,"Maddalena","Gnappi");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (10,"Pietro","Forlani");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (11,"Ludovico","Giraudo");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (12,"Tommaso","Stefanini");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (13,"Yanghuan","Yu");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (14,"Daniele","Oltrecolli");
+
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (NULL,"Luca","Sabattini");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (NULL,"Lorenzo","Bandinelli");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (NULL,"Giovanni","Bravin");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (NULL,"Tommaso","Melandri");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (NULL,"Agnese","Faberi");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (NULL,"Giovanni","Emiliani");
+INSERT INTO `Persone` (`_id`,`nome`,`cognome`) VALUES (NULL,"Giulia","Pellicciari");
+
+
+
+
+/* Restore session variables to original values */
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
---
+++ Transazioni.sql
@@ -0,0 +1,28 @@
+/* Table data export for table Transazioni */
+
+/* Preserve session variables */
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS;
+SET FOREIGN_KEY_CHECKS=0;
+
+/* Export data */
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (1,1,3.5,"Dicembre","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (2,1,2,"Gennaio","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (3,3,3.5,"Dicembre","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (4,3,3.5,"Gennaio","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (5,4,4,"Dicembre","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (6,4,4,"Gennaio","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (7,5,3,"Dicembre","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (8,5,3,"Gennaio","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (9,6,5,"Dicembre","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (10,6,5,"Gennaio","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (11,7,2.5,"Dicembre","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (12,7,2.5,"Gennaio","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (13,8,1,"Dicembre","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (14,8,1,"Gennaio","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (15,9,4,"Dicembre","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (16,9,4,"Gennaio","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (17,10,3,"Dicembre","2018");
+INSERT INTO `Transazioni` (`_id`,`userId`,`cash`,`mese`,`anno`) VALUES (18,10,3,"Gennaio","2018");
+
+/* Restore session variables to original values */
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
